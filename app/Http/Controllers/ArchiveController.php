746f1335b1ba7b0dd288c1663a18605eea8c3543
imadelcass <imadelcass@gmail.com>
2022-03-31 13:53:52 +0200

    commit

--- app/Http/Controllers/ArchiveController.php
+++ app/Http/Controllers/ArchiveController.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use App\Models\Archive;
 class ArchiveController extends Controller
 {
-    //
+    public function index()
+    {
+        $query = Archive::query();
+        return $query->get();
+    }
+
+    public function create(Request $request)
+    {
+        $archive = new Archive();
+        $archive->codeArchive = 'f';
+        $archive->intitulArchive = 'dsf';
+        $archive->save();
+        return $archive();
+    }
 }
